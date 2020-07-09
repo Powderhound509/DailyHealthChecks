c637d5b0d62160e208ed82e61a25873a17e1773b
Joshua Lent <jolent@microsoft.com>
2020-07-09 13:47:02 -0700

    Initial Repo Clone.  Added DB Project, Imported DB and starter table.

---
+++ DailyHealthChecks/dbo/Tables/SQLUpTime.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SQLUpTime] (
+    [recordID]        INT           IDENTITY (1, 1) NOT NULL,
+    [serverName]      [sysname]     NOT NULL,
+    [serverStartTime] DATETIME2 (7) NOT NULL,
+    [recordTime]      DATETIME2 (7) DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([recordID] ASC)
+);
+
