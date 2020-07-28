aaf067ad98584e31a477dbcb78861706fdd866e3
Joshua Lent <jolent@microsoft.com>
2020-07-28 12:43:30 -0700

    Backup History and SP stub

---
+++ DailyHealthChecks/Scripts/PostDeployment.sql
@@ -0,0 +1,20 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--backupHistory Columnstore Index
+USE DailyHealthChecks
+go
+
+CREATE CLUSTERED COLUMNSTORE INDEX IX_backupStatusHistory
+    ON backupStatusHistory;
+CREATE NONCLUSTERED INDEX IX_DepartmentHistory_ID_PERIOD_COLUMNS
+    ON backupStatusHistory (SysEndTime, SysStartTime, recordId);
+GO
