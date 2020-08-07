c15408115583963ca5e78a402061af4cd863a600
Joshua Lent <joshua.lent@microsoft.com>
2020-08-07 10:13:48 -0700

    Repaired missing objects

--- DailyHealthChecks/Scripts/PostDeployment.sql
+++ DailyHealthChecks/Scripts/PostDeployment.sql
@@ -10,71 +10,62 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 --backupHistory Columnstore Index
-USE DailyHealthChecks
-go
-
 CREATE CLUSTERED COLUMNSTORE INDEX IX_backupStatusHistory
     ON backupStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_backupStatusHistory_ID_PERIOD_COLUMNS
     ON backupStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 --diskSpaceHistory Columnstore Index
-USE DailyHealthChecks
-go
 
 CREATE CLUSTERED COLUMNSTORE INDEX IX_diskSpaceHistory
     ON diskSpaceHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_diskSpaceHistory_ID_PERIOD_COLUMNS
     ON diskSpaceHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 --jobStatusHistory Columnstore Index
-USE DailyHealthChecks
-go
 
 CREATE CLUSTERED COLUMNSTORE INDEX IX_jobStatusHistory
     ON jobStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_jobStatusHistory_ID_PERIOD_COLUMNS
     ON jobStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 --serviceStatusHistory Columnstore Index
-USE DailyHealthChecks
-go
 
 CREATE CLUSTERED COLUMNSTORE INDEX IX_serviceStatusHistory
     ON serviceStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_serviceStatusHistory_ID_PERIOD_COLUMNS
     ON serviceStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 --clusterStatusHistory Columnstore Index
-USE DailyHealthChecks
-go
 
 CREATE CLUSTERED COLUMNSTORE INDEX IX_clusterStatusHistory
     ON clusterStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_clusterStatusHistory_ID_PERIOD_COLUMNS
     ON clusterStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
 -- SERVER UPTIME Columnstore Index
 CREATE CLUSTERED COLUMNSTORE INDEX IX_SQLUpTimeHistory
     ON SQLUpTimeHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_SQLUpTimeHistory_ID_PERIOD_COLUMNS
     ON SQLUpTimeHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 -- DATABASE STATUS Columnstore Index
 CREATE CLUSTERED COLUMNSTORE INDEX IX_DatabaseStatusHistory
     ON DatabaseStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_DatabaseStatusHistory_ID_PERIOD_COLUMNS
     ON DatabaseStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
-
 -- AGStatus Columnstore Index
 CREATE CLUSTERED COLUMNSTORE INDEX IX_AGStatusHistory
     ON AGStatusHistory;
+GO
 CREATE NONCLUSTERED INDEX IX_AGStatusHistory_ID_PERIOD_COLUMNS
     ON AGStatusHistory (SysEndTime, SysStartTime, recordId);
 GO
---
+++ DailyHealthChecks/dbo/Tables/diskSpaceHistory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[diskSpaceHistory]
+(
+[recordId] [int] NOT NULL,
+	[server_name] [nvarchar](128) NOT NULL,
+	[volume_mount_point] [nvarchar](256) NULL,
+	[logical_volume_name] [nvarchar](256) NULL,
+	[total_size_gb] [decimal](18, 2) NULL,
+	[available_size_gb] [decimal](18, 2) NULL,
+	[free_space_pct] [decimal](18, 2) NULL,
+	[SysStartTime] [datetime2](7) NOT NULL,
+	[SysEndTime] [datetime2](7) NOT NULL
+)
