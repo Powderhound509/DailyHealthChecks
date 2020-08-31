e4f77733289838ae0c92c0bad3c5171f3e118b84
Joshua Lent <mr.lent@gmail.com>
2020-08-31 10:44:17 -0700

    Added SetupJob post deployment script

---
+++ DailyHealthChecks/Scripts/PostDeployment.SetupJob.sql
@@ -0,0 +1,58 @@
+﻿/*					
+--------------------------------------------------------------------------------------
+Sets up a SQL Agent Job to execute the DailyHealthCheck				
+--------------------------------------------------------------------------------------
+*/
+USE [msdb]
+GO
+
+/****** Object:  Job [DailyHealthChecks]    Script Date: 8/31/2020 9:34:45 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Data Collector]    Script Date: 8/31/2020 9:34:45 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DailyHealthChecks', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'Data Collector', 
+--		@owner_login_name=select suser_sname(0x01), 
+		@job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Invoke-DailyHealthChecks]    Script Date: 8/31/2020 9:34:45 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Invoke-DailyHealthChecks', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'CmdExec', 
+		@command=N'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "C:\SQLDailyHealthChecks\Invoke-MorningHealthChecks.ps1" -cmsServer "myCMS" -cmsGroup "myCMSGroup" -cmsDatabase "myCMSDatabase"', 
+		@flags=0, 
+		@proxy_name=N'STIG_Proxy'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
